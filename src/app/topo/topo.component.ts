083317da2a0f336ff42aa249a6269ef3d96261c5
Emilyn Pieritz <66131506+EPieritz@users.noreply.github.com>
2020-10-06 10:40:12 -0300

    Add files via upload

---
+++ src/app/topo/topo.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-topo',
+  templateUrl: './topo.component.html',
+  styleUrls: ['./topo.component.css'],
+})
+export class TopoComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
